a7ecec7237197a805aad61e4cd8e24c64e57c102
andyglick <andyglick@gmail.com>
2017-02-17 23:13:24 -0500

    removed bogus Java classes created by the maven artifact, added a demo Java class that has errors found by the compioler

---
+++ src/main/java/org/zrgs/errorprone/ShortSet.java
@@ -0,0 +1,17 @@
+package org.zrgs.errorprone;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ShortSet {
+  public static void main (String[] args) {
+    Set<Short> s = new HashSet<>();
+
+    for (short i = 0; i < 100; i++) {
+      s.add(i);
+      s.remove(i - 1);
+    }
+
+    System.out.println(s.size());
+  }
+}
