47c4f2dec6f62c36c6af9f69766088f2bec39b28
Andy Glick <andyglick@gmail.com>
2017-12-24 08:21:56 -0500

    bumped versions of dependecies and plugins, migrated the ShortSet class which ran as a "main" to a junit test

---
+++ src/test/java/org/zrgs/errorprone/ShortSetTest.java
@@ -0,0 +1,47 @@
+package org.zrgs.errorprone;
+
+import org.junit.Test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.invoke.MethodHandles;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * ShortSetTest is a demonstration class to show what kinds of problems the errorprone
+ * compiler can identify and report with existing Java code
+ *
+ * the code below compiles, but it has been modified from the original example
+ *
+ * what didn't compile was the statement
+ *
+ * s.remove(i - 1);
+ *
+ * as it resolved to int and not to short
+ */
+public class ShortSetTest
+{
+  private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+  @Test
+  public void testShortSet()
+  {
+
+    Set<Short> s = new HashSet<>();
+    final short ONE = 1;
+
+    for (short i = 0; i < 100; i++)
+    {
+      s.add(i);
+      s.remove((short) (i - ONE));
+    }
+
+    log.info("the size  of the set is " + s.size());
+
+    assertThat(s.size()).isEqualTo(1);
+  }
+}
